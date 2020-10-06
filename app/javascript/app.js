46ca9ae351c63c1069ee0e42f2c74e8551fdea5d
Saket Yadav <saketkumaryadav@gmail.com>
2020-10-06 15:41:26 +0530

    Update app.js
    Page path has been updated

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,25 +1,25 @@
 $(function(){
-    $('#innerpage').load("/app/pages/home.html");
+    $('#innerpage').load("/limelightnss/app/pages/home.html");
 
     $("#daily_running_news_list").on("click",function(){
-        $('#innerpage').load("/app/pages/daily_running_news_list.html");
+        $('#innerpage').load("/limelightnss/app/pages/daily_running_news_list.html");
 
     });
     $("#home").on("click",function(){
-        $('#innerpage').load("/app/pages/home.html");
+        $('#innerpage').load("/limelightnss/app/pages/home.html");
 
     });
 
     $("#important_blog").on("click",function(){
-        $('#innerpage').load("/app/pages/important_blog.html");
+        $('#innerpage').load("/limelightnss/app/pages/important_blog.html");
 
     });
     $("#important_site_path").on("click",function(){
-        $('#innerpage').load("/app/pages/important_site_path.html");
+        $('#innerpage').load("/limelightnss/app/pages/important_site_path.html");
 
     });
     $("#travel_related_site_in_india").on("click",function(){
-        $('#innerpage').load("/app/pages/travel_related_site_in_india.html");
+        $('#innerpage').load("/limelightnss/app/pages/travel_related_site_in_india.html");
 
     });
 });
